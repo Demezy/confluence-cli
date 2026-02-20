cdb669211e8cabf10c78f8a72aa07a8e18b415e1
Demezy <38487319+Demezy@users.noreply.github.com>
2026-02-20 21:31:09 +0300

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A command-line interface for Atlassian Confluence";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        confluence-cli = pkgs.buildNpmPackage {
+          pname = "confluence-cli";
+          version =
+            (builtins.fromJSON (builtins.readFile ./package.json)).version;
+
+          src = ./.;
+
+          npmDepsHash = "sha256-19JlVbW2c1U16tgk6w7f5NJTVpkpYE2zaIUUGHaElSs=";
+
+          dontNpmBuild = true;
+
+          meta = {
+            description = "A command-line interface for Atlassian Confluence";
+            homepage = "https://github.com/pchuri/confluence-cli";
+            license = pkgs.lib.licenses.mit;
+            mainProgram = "confluence";
+          };
+        };
+        default = self.packages.${pkgs.system}.confluence-cli;
+      });
+    };
+}
